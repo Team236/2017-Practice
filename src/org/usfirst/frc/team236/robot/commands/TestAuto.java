3f689b2152e0deaf9c68877ced6bfd41da902789
Sam Fuchs <samcfuchs@gmail.com>
2017-01-29 15:40:20 -0500

    Remove turn at end of straight auto

--- src/org/usfirst/frc/team236/robot/commands/TestAuto.java
+++ src/org/usfirst/frc/team236/robot/commands/TestAuto.java
@@ -1,13 +1,8 @@
 package org.usfirst.frc.team236.robot.commands;
 
-import org.usfirst.frc.team236.robot.AutoMap;
-import org.usfirst.frc.team236.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import ticktank.Direction;
 import ticktank.TickTank;
 import ticktank.commands.FollowProfile;
-import ticktank.commands.Turn;
 import ticktank.motionProfile.Profile;
 
 /**
@@ -17,6 +12,5 @@ public class TestAuto extends CommandGroup {
 
 	public TestAuto(TickTank _tank, Profile p) {
 		addSequential(new FollowProfile(_tank, p, false));
-		addSequential(new Turn(Robot.tank, AutoMap.rightGearDegrees, Direction.CCW));
 	}
 }
